b96c69907b3c4c2b8333e3f8ea7ac8d5813faea5
Superjake03 <jacoblopez0308@gmail.com>
2025-10-23 15:57:06 -0500

    reading in user prompts

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("No prompt provided\n")
+        print('Usage: python main.py "your prompt here"')
+        sys.exit(1)
+    prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=prompt,
+    )
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()

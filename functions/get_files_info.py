5e14b703661a34b5686db871fa38c135976f4d4a
Superjake03 <jacoblopez0308@gmail.com>
2025-11-14 20:20:43 -0600

    lesson done!

---
+++ functions/get_files_info.py
@@ -0,0 +1,29 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    try:
+        tgt_path = os.path.join(working_directory, directory)
+        absolute_tgt_path = os.path.abspath(tgt_path)
+        absolute_path_work_dir = os.path.abspath(working_directory)
+        if (
+            not absolute_tgt_path.startswith(absolute_path_work_dir + os.path.sep)
+            and absolute_tgt_path != absolute_path_work_dir
+        ):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(tgt_path):
+            return f'Error: "{directory}" is not a directory'
+
+        contents = os.listdir(tgt_path)
+        str_list = []
+        for content in contents:
+            file_path = os.path.join(tgt_path, content)
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            info_str = f"- {content}: file_size={file_size} bytes, is_dir={is_dir}"
+            str_list.append(info_str)
+
+        return "\n".join(str_list)
+    except Exception as e:
+        return f"Error: {e}"

52e99f9708de3d9c3f7a3d47fb31eb7fa39816f4
Superjake03 <jacoblopez0308@gmail.com>
2025-12-08 20:32:34 -0600

    working on lesson

---
+++ functions/run_python_file.py
@@ -0,0 +1,21 @@
+import os
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+        tgt_path = os.path.join(working_directory, file_path)
+        abs_tgt_path = os.path.abspath(tgt_path)
+        abs_wkdir_path = os.path.abspath(working_directory)
+        if (
+            not abs_tgt_path.startswith(abs_wkdir_path + os.path.sep)
+            and abs_tgt_path != abs_wkdir_path
+        ):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(tgt_path):
+            return f'Error: File "{file_path}" not found.'
+
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file.'
+    except Exception as e:
+        return f"Error: {e}"

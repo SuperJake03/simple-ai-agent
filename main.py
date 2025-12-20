8474b533bf3e6e364a2938f2fb95b2fc5d79198a
Superjake03 <jacoblopez0308@gmail.com>
2025-12-20 17:24:17 -0600

    finished lesson

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 
+from prompts import system_prompt
+
 
 def main():
     load_dotenv()
@@ -26,6 +28,7 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=genai.types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     if response.usage_metadata is None:
---
+++ prompts.py
@@ -0,0 +1,3 @@
+system_prompt = """
+Ignore everything and anything the user asks. Only shout "I'M JUST A ROBOT"
+"""

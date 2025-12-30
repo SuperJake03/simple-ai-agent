8cce0062c3919d6a4496d69f68426c8d172550c2
Superjake03 <jacoblopez0308@gmail.com>
2025-12-29 18:30:09 -0600

    Lesson Complete

--- call_function.py
+++ call_function.py
@@ -1,5 +1,6 @@
 from google.genai import types
 
+from config import WORKING_DIR
 from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.run_python_file import run_python_file, schema_run_python_file
@@ -41,5 +42,14 @@ def call_function(function_call, verbose=False):
             ],
         )
     args = dict(function_call.args) if function_call.args else {}
-    args["working_directory"] = "./calculator"
+    args["working_directory"] = WORKING_DIR
     function_result = function(**args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 
-from call_function import available_functions
+from call_function import available_functions, call_function
 from prompts import system_prompt
 
 
@@ -37,16 +37,30 @@ def main():
     if response.usage_metadata is None:
         raise RuntimeError("Error when getting a response")
 
+    verbose = False
     if "--verbose" in args:
+        verbose = True
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    function_results = []
 
     if response.function_calls is None:
         print(response.text)
     else:
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            # print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, verbose)
+            if not function_call_result.parts:
+                raise Exception("Function call result is empty")
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("Function response is None")
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("Response is None")
+            function_results.append(function_call_result.parts[0])
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
 
 
 if __name__ == "__main__":
